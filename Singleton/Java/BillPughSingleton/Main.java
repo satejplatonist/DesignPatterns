fde09faddb980fc4a1d81290ba30e234e9243ac2
SATEJ DHAKANE <113412705+satejplatonist@users.noreply.github.com>
2023-12-27 14:16:57 +0530

    Update Main.java

--- Singleton/Java/BillPughSingleton/Main.java
+++ Singleton/Java/BillPughSingleton/Main.java
@@ -9,6 +9,12 @@ public class Main
 	  BillPughSingleton instance1 = BillPughSingleton.getInstance();
 	  BillPughSingleton instance2 = BillPughSingleton.getInstance();
 	  BillPughSingleton instance3 = BillPughSingleton.getInstance();
+
+	  // all instances (instance1, instance2, instance3) are pointing to 
+	  // single object of BillPughSingleton class using HelperSingletonClass inner - static class (another way)
+	  BillPughSingleton1 instance4 = BillPughSingleton1.getInstance();
+	  BillPughSingleton1 instance5 = BillPughSingleton1.getInstance();
+	  BillPughSingleton1 instance6 = BillPughSingleton1.getInstance();
    }
 }
 
