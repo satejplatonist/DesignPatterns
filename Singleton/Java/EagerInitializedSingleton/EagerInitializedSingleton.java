7a8a24853acef16c12ef15b389912efa2fcac82c
SATEJ DHAKANE <113412705+satejplatonist@users.noreply.github.com>
2023-12-27 11:37:56 +0530

    Create EagerInitializedSingleton.java

---
+++ Singleton/Java/EagerInitializedSingleton/EagerInitializedSingleton.java
@@ -0,0 +1,12 @@
+package designPatterns;
+
+public class EagerInitializedSingleton 
+{
+   private static EagerInitializedSingleton instance = new EagerInitializedSingleton();
+   private EagerInitializedSingleton() {}
+   
+   public static EagerInitializedSingleton getInstance()
+   {
+	   return instance;
+   }
+}
