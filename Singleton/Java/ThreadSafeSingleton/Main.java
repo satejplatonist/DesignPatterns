cee1f499bf3074832b1b2e7adfc03fd0c43e177f
SATEJ DHAKANE <113412705+satejplatonist@users.noreply.github.com>
2023-12-27 12:56:41 +0530

    Update Main.java

--- Singleton/Java/ThreadSafeSingleton/Main.java
+++ Singleton/Java/ThreadSafeSingleton/Main.java
@@ -9,6 +9,12 @@ public class Main
 	  ThreadSafeSingleton instance1 = ThreadSafeSingleton.getInstance();
 	  ThreadSafeSingleton instance2 = ThreadSafeSingleton.getInstance();
 	  ThreadSafeSingleton instance3 = ThreadSafeSingleton.getInstance();
+
+	  // all instances (instance1, instance2, instance3) are pointing to 
+	  // single object of ThreadSafeSingleton with double-checked locking
+	  ThreadSafeSingleton instance1 = ThreadSafeSingleton.getInstanceDoubleChechedLocking();
+	  ThreadSafeSingleton instance2 = ThreadSafeSingleton.getInstanceDoubleChechedLocking();
+	  ThreadSafeSingleton instance3 = ThreadSafeSingleton.getInstanceDoubleChechedLocking();
    }
 }
 
