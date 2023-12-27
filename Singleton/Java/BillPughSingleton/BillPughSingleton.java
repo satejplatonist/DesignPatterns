b6709c158dd5dc648b6aacebcdad468e5c7a4022
SATEJ DHAKANE <113412705+satejplatonist@users.noreply.github.com>
2023-12-27 14:09:04 +0530

    Create BillPughSingleton.java

---
+++ Singleton/Java/BillPughSingleton/BillPughSingleton.java
@@ -0,0 +1,16 @@
+package designPatterns;
+
+public class BillPughSingleton 
+{
+    private BillPughSingleton() {}
+    
+    private static class HelperSingletonClass
+    {
+    	private static final BillPughSingleton instance = new BillPughSingleton();
+    }
+    
+    public static BillPughSingleton getInstance()
+    {
+    	return HelperSingletonClass.instance;
+    }
+}
